efd2c4a297f09375db461a6858ea33a3f5ac0cfa
Chuong Tran <chuongninolxag@gmail.com>
2021-12-04 21:00:58 +0700

    add side bar, function login, logout

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,78 @@
-import NextAuth from "next-auth"
-import SpotifyProvider from "next-auth/providers/spotify"
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshToken();
+    console.log("refreshedToken is: ", refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+
+      // spotify API
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+      //replace if new one came back else fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(
+      "🚀 ~ file: [...nextauth].js ~ line 10 ~ refreshAccessToken ~ error",
+      error
+    );
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
-})
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ account, token, user }) {
+      //init sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          userName: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      //return previous token if access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("EXISTING ACCESS TOKEN IS VALID");
+        return token;
+      }
+
+      //access token has expired, so we need to refresh it
+      console.log("ACCESS TOKEN  HAS EXPIRED, REFRESHING...");
+
+      return await refreshAccessToken(token);
+    },
+  },
+
+  async session({ session, token }) {
+    session.user.accessToken = token.accessToken;
+    session.user.refreshToken = token.refreshToken;
+    session.user.username = token.username;
+
+    
+    return session;
+  },
+});
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,57 @@
-function Login() {
-    return (
-        <div>
-            this is login page
-        </div>
-    )
+import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
+
+import spotifyImage from "../static/images/spotify-logo.png";
+
+function Login(props) {
+  const { providers } = props;
+
+  const handleOnLogin = (provider) => {
+    signIn(provider.id, { callbackUrl: "/" });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full">
+      <div
+        style={{
+          minHeight: "96px",
+          width: "13rem",
+          position: "relative",
+        }}
+        className="w-52 mb-5"
+      >
+        <Image
+          layout="fill"
+          objectFit="contain"
+          src={spotifyImage}
+          className="pointer"
+          alt="spotify logo"
+        />
+      </div>
+      {Object.values(providers).map((provider) => {
+        return (
+          <div key={provider.name}>
+            <button
+              className="bg-[#18D860] text-white p-5 rounded-full"
+              onClick={() => handleOnLogin(provider)}
+            >
+              Login with {provider.name}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
